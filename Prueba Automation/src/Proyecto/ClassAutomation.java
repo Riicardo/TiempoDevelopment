47772929aacb23d47699a7e910256945350a8d8e
Ricardo <riicardo.ovalles@gmail.com>
2019-09-11 18:12:16 -0500

    Ejercicio Tiempo Development

---
+++ Prueba Automation/src/Proyecto/ClassAutomation.java
@@ -0,0 +1,16 @@
+package Proyecto;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClassAutomation {
+
+		public static void main(String[] args) {
+		
+		//WebDriver driver = new ChromeDriver();
+
+		WebDriver driver = new ChromeDriver();
+		}
+
+	
+}
